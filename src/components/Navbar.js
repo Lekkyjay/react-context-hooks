51d28edf8408aa64e26ec5d9bf0bbb51807b2c0b
Lekkyjay <lekkyjayjay@yahoo.com>
2020-06-13 20:33:46 +0200

    Implemented passing data  with context.Consumer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-  //this looks up the component tree for the provider of ThemeContext and acces it's value prop.
-  static contextType = ThemeContext
   render() { 
-    console.log(this.context)
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return ( 
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+        return (
+          <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        )
+      }}</ThemeContext.Consumer>
     );
   }
 }
